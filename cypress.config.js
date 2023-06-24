aa13ffdec472e454b21af4efca88e06f859bcc92
Johnny Kamigashima <johnny@jhk.app>
2023-06-24 13:14:48 -0300

    Adicionando allure plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
     baseUrl: "https://www.google.com",
     retries: {
       runMode: 1,
       openMode: 4,
-      },
+    },
     viewportHeight: 1080,
     viewportWidth: 1920,
   },
